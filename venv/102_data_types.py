8d90e5184251533cb78c36c9054eb0458abc3d9f
Jillian Guillen <JGuillen@spartaglobal.com>
2019-09-23 11:03:30 +0100

    finished numerical, moving into booleans

--- venv/102_data_types.py
+++ venv/102_data_types.py
@@ -3,7 +3,8 @@
 # These are numerical data types which we can use numerical operators
 
 # Complex and long we don't use as much
-# 
+# Complex bring an imaginary type of number
+# long - are integers of unlimited size
 
 
 # int - stands for integers
@@ -11,3 +12,32 @@
 my_int = 10
 print(my_int)
 print(type(my_int))
+
+#Operator - add, subtract, divide, multiply and exponential
+print(4 + 3)
+print(5 - 1)
+print(4/2) # Divisions automatically get converted to float
+print(4//2) #Double divide drops the float
+print(4*2)
+print(3**2)
+
+#Modulus finds out the remainder
+print(10%3)
+print(22%3)
+
+# Comparison operators
+# = - Assignment
+# == - Equal to
+# < / > - Greater and less than
+# <= - Less than or equal to
+# >= - Greater than or equal to
+# != - Not equal to
+# is / is not
+
+my_variable1 = 10
+my_variable2 = 13
+
+print(my_variable1 == my_variable2)
+print(my_variable2 > my_variable1)
+
+# Boolean Values 
