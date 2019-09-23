e0f70ae77a4adfc1c2cbef2527cf75b732163422
Jillian Guillen <JGuillen@spartaglobal.com>
2019-09-23 13:55:43 +0100

    casted something into a str() and a int()

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -10,12 +10,12 @@ hair_color = 'Black'
 # Prompt user for input and re-assign these
 name = input ('What is your name?   ').capitalize()
 last_name = input ('What is your last name?   ').capitalize()
-age = input ('How old are you?   ')
+age = int(input ('How old are you?   '))
 eye_color = input ('What color are your eyes?     ').lower()
 hair_color = input('What color is your hair?     ').lower()
 
 # Print them back to the user as conversation
-print(f'Hey there, {name} {last_name}, you are {age}. The color of your eyes are {eye_color} and the color of your hair is {hair_color}. Very nice!')
+print(f'Hey there {name} {last_name}, you are {age}. The color of your eyes are {eye_color} and the color of your hair is {hair_color}. Very nice!')
 # Example: 'Hello Jack! Welcome, your age is 26, you eyes are green and your hair is black.'
 
 # Section 2 - Calculate in what year was the person born? And respond back.
