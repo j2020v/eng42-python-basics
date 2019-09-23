56d01565267a0a8ae2b5f17b5c8760c27e28078a
Jillian Guillen <JGuillen@spartaglobal.com>
2019-09-23 10:45:03 +0100

    looked at variables, datatypes and functions

---
+++ 101_variables_basic_commands.py
@@ -0,0 +1,33 @@
+# Variable in Python
+# Variables is a box. You can give it a name and put stuff inside
+
+#Assigning a Variable
+# Giving box a name and putting stuff inside
+box_variable = 'Books and stuff'
+
+# Looking inside the box
+print('box_variable')
+
+#Re Assinging a Variable
+# Variables are mutable - Hence they can change/be re-assigned
+box_variable = "CD's and other stuff"
+print(box_variable)
+
+#Re Assigning Variable with an integer
+# Variables can hold any data type inside
+box_variable = 14
+print(box_variable)
+
+# Important python function
+# print(arguments)
+print(box_variable) # printing string
+print('Hello', box_variable) #overloading with two arguments
+
+# type()
+# output the data type of an object
+print(type('hello'))
+print(type(14))
+print(type(14.0))
+
+variable_number = '14'
+# print (type('10'*variable_number)) THIS WILL BREAK -- DATATYPES ARE KEY --
---
+++ venv/102_data_types.py
@@ -0,0 +1,13 @@
+# Numerical Data Types
+# -Ints, long, float, complex
+# These are numerical data types which we can use numerical operators
+
+# Complex and long we don't use as much
+# 
+
+
+# int - stands for integers
+# Whole numbers
+my_int = 10
+print(my_int)
+print(type(my_int))
